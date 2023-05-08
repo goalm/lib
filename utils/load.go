13d7eb54587e3aee7453341adb559ff025ae5429
Martin <martin@pku.edu.cn>
2023-05-08 10:12:48 +0800

    Reverse change to load.go

--- utils/load.go
+++ utils/load.go
@@ -33,3 +33,53 @@ func LoadCsv[T any](fileName string, row T) []*T {
 
 	return data
 }
+
+func ParseCsvMap(filename string) (map[string][]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	keys := records[0]
+	stringMap := make(map[string][]string, len(records)-1)
+	for _, record := range records[1:] {
+		for j, val := range record {
+			if val != "" {
+				stringMap[keys[j]] = append(stringMap[keys[j]], val)
+			}
+
+		}
+	}
+
+	return stringMap, nil
+}
+
+func MergeStringSlices(slices ...[]string) []string {
+	uniqueStrings := make(map[string]bool)
+	var mergedSlice []string
+	for _, slice := range slices {
+		for _, str := range slice {
+			if !uniqueStrings[str] {
+				uniqueStrings[str] = true
+				mergedSlice = append(mergedSlice, str)
+			}
+		}
+	}
+	return mergedSlice
+}
+
+func StringExists(s []string, t string) bool {
+	for _, str := range s {
+		if str == t {
+			return true
+		}
+	}
+	return false
+}
